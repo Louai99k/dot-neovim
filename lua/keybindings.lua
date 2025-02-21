0a358fefe78695f4d7d7d9c7b0fe2cf2fe4269c2
Louai99k <louaikelani99k@gmail.com>
2025-02-21 23:30:43 +0300

    replace paste with yank with paste without yank

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader><CR>", "o<ESC>", { desc = "Insert new line" })
-
 vim.keymap.set("i", "<C-v>", "<C-c>pa", { desc = "Make Ctrl+v paste" })
 vim.keymap.set("v", "<leader>s", "<ESC>/\\%V", { desc = "Search in selected area" })
+vim.keymap.set("v", "p", "P", { desc = "Make default pasting without yanking" })
+vim.keymap.set("v", "P", "p", { desc = "Make capital P pasting with yanking" })
