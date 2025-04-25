9d9dd2cc30b89bfa8aa083fa5361463b1e98cdcf
Louai99k <louaikelani99k@gmail.com>
2025-04-25 14:17:02 +0300

    set keybinding for line wrapping

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -7,3 +7,4 @@ vim.keymap.set("v", "<leader>s", "<ESC>/\\%V", { desc = "Search in selected area
 vim.keymap.set("v", "p", "P", { desc = "Make default pasting without yanking" })
 vim.keymap.set("v", "P", "p", { desc = "Make capital P pasting with yanking" })
 vim.keymap.set("n", "<leader>s", ":nohlsearch<CR>", { desc = "No highlighting for search", noremap = true })
+vim.keymap.set("n", "<A-w>", ":set wrap!<CR>", { desc = "Toggle line wrap", noremap = true })
